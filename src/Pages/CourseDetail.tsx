8c0d773416f2c59bd10a638e8154d87f4a69cb46
vinisp <aragao93.vinicius@gmail.com>
2022-03-02 19:24:07 -0300

    about course Page

--- src/Pages/CourseDetail.tsx
+++ src/Pages/CourseDetail.tsx
@@ -1,12 +1,74 @@
 import { useParams } from "react-router-dom";
+import data from "../backendFake/allcourses.json";
+import { styled } from "@mui/material/styles";
+
+const TitleSection = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-around",
+  background: "#f2f2f2",
+  padding: "40px 0",
+  borderBottom: "solid 2px silver",
+
+  [theme.breakpoints.down("sm")]: {
+    display: "flex",
+  },
+  [theme.breakpoints.up("sm")]: {},
+
+  [theme.breakpoints.up("md")]: {},
+  [theme.breakpoints.up("lg")]: {
+    img: {
+      width: "450px",
+    },
+  },
+}));
+const ModulesSection = styled("div")(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  background: "#f2f2f2",
+
+  padding: "40px 0",
+
+  [theme.breakpoints.down("sm")]: {
+    display: "flex",
+  },
+  [theme.breakpoints.up("sm")]: {},
+
+  [theme.breakpoints.up("md")]: {},
+  [theme.breakpoints.up("lg")]: {},
+}));
 
 export function CourseDetail() {
   const { idCourse } = useParams<any>();
+  const selectCourse = data.filter((e) => e.id === +idCourse);
+  console.log(selectCourse);
 
   return (
     <>
-      <h1>Course Detail Page</h1>
-      <h3> {idCourse}</h3>
+      {selectCourse.map((e) => (
+        <>
+          <TitleSection>
+            <div key={e.id}>
+              <h1> {e.title} </h1>
+              <h3>{e.courseAbout}</h3>
+            </div>
+            <div>
+              <img src={e.image} alt="texto sobre a imagem" />
+            </div>
+          </TitleSection>
+          <div>
+            {e.modules?.map((module) => (
+              <>
+                <ModulesSection>
+                  <h4> {module.moduleTitle} </h4>
+                  <p>{module.moduleResume} </p>
+                </ModulesSection>
+              </>
+            ))}
+          </div>
+        </>
+      ))}
     </>
   );
 }
