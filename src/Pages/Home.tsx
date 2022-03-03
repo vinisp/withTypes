dd98bc633ef97f8151ec2dc5d4cc6619887562c7
vinisp <aragao93.vinicius@gmail.com>
2022-03-03 17:38:24 -0300

    reduzir elementos

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,8 +1,6 @@
 import Hero from "../Components/sections/Hero";
 import Results from "../Components/sections/Results";
-import Main from "../Components/sections/Main";
 import Plans from "../Components/sections/Plans";
-import AboutPlanosSection from "../Components/sections/AboutPlans";
 import Faq from "../Components/sections/Faq";
 import { useContext } from "react";
 
@@ -16,9 +14,7 @@ function Home() {
     <>
       <Hero />
       <Results />
-      <Main />
       <Plans />
-      <AboutPlanosSection />
       <Faq />
     </>
   );
