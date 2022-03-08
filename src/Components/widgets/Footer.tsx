80ab680c40f7a6511ea0162e695ff713e1fb9edf
vinisp <aragao93.vinicius@gmail.com>
2022-03-08 11:25:05 -0300

    footer #2

--- src/Components/widgets/Footer.tsx
+++ src/Components/widgets/Footer.tsx
@@ -1,8 +1,12 @@
 import { styled } from "@mui/material/styles";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import TwitterIcon from "@mui/icons-material/Twitter";
 
 const FooterStyled = styled("footer")(({ theme }) => ({
   background: "#26272b",
-  padding: "45px 20px 40px 55px",
+  padding: "15px 0",
   fontSize: "15px",
   lineHeight: "24px",
   color: "#737373",
@@ -11,6 +15,12 @@ const FooterStyled = styled("footer")(({ theme }) => ({
   height: "100%",
 
   width: "100%",
+  ul: {
+    listStyle: "none",
+  },
+  h4: {
+    color: "white",
+  },
   [theme.breakpoints.down("sm")]: {},
   [theme.breakpoints.up("sm")]: {},
 
@@ -20,8 +30,9 @@ const FooterStyled = styled("footer")(({ theme }) => ({
 
 const ContainerFooter = styled("footer")(({ theme }) => ({
   display: "flex",
+  gap: "35px",
 
-  padding: "15px 0px",
+  padding: "20px 0px 15px 35px",
   margin: 0,
   borderBottom: " solid 1px #737373",
 
@@ -34,7 +45,7 @@ const ContainerFooter = styled("footer")(({ theme }) => ({
 }));
 
 const AboutText = styled("footer")(({ theme }) => ({
-  flex: "0 0 40%",
+  flex: "0 0 30%",
   padding: 0,
   margin: 0,
 
@@ -52,6 +63,7 @@ const PoliticsFooterBlock = styled("footer")(({ theme }) => ({
   margin: 0,
 
   width: "100%",
+
   [theme.breakpoints.down("sm")]: {},
   [theme.breakpoints.up("sm")]: {},
 
@@ -78,6 +90,24 @@ const SocialFooterBlock = styled("footer")(({ theme }) => ({
   margin: 0,
 
   width: "100%",
+
+  ul: {
+    display: "flex",
+    gap: "15px",
+  },
+  [theme.breakpoints.down("sm")]: {},
+  [theme.breakpoints.up("sm")]: {},
+
+  [theme.breakpoints.up("md")]: {},
+  [theme.breakpoints.up("lg")]: {},
+}));
+
+const CopyFooterBlock = styled("footer")(({ theme }) => ({
+  padding: "15px 35px",
+  margin: 0,
+
+  width: "100%",
+
   [theme.breakpoints.down("sm")]: {},
   [theme.breakpoints.up("sm")]: {},
 
@@ -91,7 +121,7 @@ export function Footer() {
       <FooterStyled>
         <ContainerFooter>
           <AboutText>
-            <h5>About</h5>
+            <h4>SOBRE</h4>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
               nobis neque, temporibus modi eum nesciunt quos, quisquam, repellat
@@ -99,11 +129,42 @@ export function Footer() {
               aperiam repudiandae minus.
             </p>
           </AboutText>
-          <PoliticsFooterBlock>POLÍTICAS</PoliticsFooterBlock>
-          <ContactsFooterBlock>CONTATOS</ContactsFooterBlock>
-          <SocialFooterBlock>REDES SOCIAIS</SocialFooterBlock>
+          <PoliticsFooterBlock>
+            <h4>POLÍTICAS</h4>
+            <ul>
+              <li>Link 1</li>
+              <li>Link 2</li>
+              <li>Link 3</li>
+              <li>Link 4</li>
+            </ul>
+          </PoliticsFooterBlock>
+          <ContactsFooterBlock>
+            <h4>CONTATOS</h4>
+            <ul>
+              <li>Contato 1</li>
+              <li>Contato 2</li>
+              <li>Contato 3</li>
+            </ul>
+          </ContactsFooterBlock>
+          <SocialFooterBlock>
+            <h4> REDES SOCIAIS </h4>
+            <ul>
+              <li>
+                <FacebookIcon />
+              </li>
+              <li>
+                <InstagramIcon />
+              </li>
+              <li>
+                <WhatsAppIcon />
+              </li>
+              <li>
+                <TwitterIcon />
+              </li>
+            </ul>
+          </SocialFooterBlock>
         </ContainerFooter>
-        <p> &copy; Todos os direitos reservados</p>
+        <CopyFooterBlock> &copy; Todos os direitos reservados</CopyFooterBlock>
       </FooterStyled>
     </>
   );
