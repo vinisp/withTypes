f26256af6acfbc4d1760a737e249ee483621d4f6
vinisp <aragao93.vinicius@gmail.com>
2022-03-08 15:18:43 -0300

    checkout page

--- src/Components/widgets/NewCartApp/Checkout.tsx
+++ src/Components/widgets/NewCartApp/Checkout.tsx
@@ -7,17 +7,23 @@
 //GERAR ID DA OPERAÇÃO
 
 import { useContext, useState } from "react";
+import { Link } from "react-router-dom";
 import { CartContext } from "./CartContext";
 import { CartItemType } from "./NewCartApp";
-
+import { Footer } from "../Footer";
+import { Button } from "@mui/material";
+import { useAuth } from "../../../hooks/useAuth";
 import { styled } from "@mui/material/styles";
 
 import "./Styles/Checkout.css";
-import { Button } from "@mui/material";
 
-const CheckoutWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(1),
+const CheckoutMain = styled("div")(({ theme }) => ({
+  padding: "60px 0",
   background: "#f2f2f2",
+  display: "grid",
+  gridTemplateColumns: "repeat(12, 1fr)",
+  gridAutoRows: "80px",
+  gap: "2rem",
 
   [theme.breakpoints.down("sm")]: {
     height: "450px",
@@ -33,12 +39,115 @@ const CheckoutWrapper = styled("div")(({ theme }) => ({
     width: "30%",
   },
   [theme.breakpoints.up("lg")]: {
-    height: "650px",
-    width: "20%",
+    height: "900px",
+    width: "100%",
+  },
+}));
+
+const CheckoutWrapper = styled("div")(({ theme }) => ({
+  gridColumn: "2 / 8",
+  gridRow: "2 / 8",
+  padding: "120px 0",
+  background: "#F2F2F2",
+  borderRadius: "8px",
+  boxShadow: "0 0 10px 2px rgba(0,0,0, 0.2)",
+
+  [theme.breakpoints.down("sm")]: {
+    height: "450px",
+    width: "95%",
+  },
+  [theme.breakpoints.up("sm")]: {
+    height: "450px",
+    width: "80%",
+  },
+
+  [theme.breakpoints.up("md")]: {
+    height: "450px",
+    width: "30%",
+  },
+  [theme.breakpoints.up("lg")]: {
+    height: "100%",
+    width: "100%",
+  },
+}));
+
+const CheckoutBoxLoginInfo = styled("div")(({ theme }) => ({
+  gridColumn: "2 / 8",
+  gridRow: "1 / 2",
+
+  background: "#F2F2F2",
+  borderRadius: "8px",
+  boxShadow: "0 0 10px 2px rgba(0,0,0, 0.2)",
+
+  [theme.breakpoints.down("sm")]: {
+    height: "100%",
+    width: "95%",
+  },
+  [theme.breakpoints.up("sm")]: {
+    height: "100%",
+    width: "80%",
+  },
+
+  [theme.breakpoints.up("md")]: {
+    height: "100%",
+    width: "30%",
+  },
+  [theme.breakpoints.up("lg")]: {
+    height: "100%",
+    width: "100%",
+  },
+}));
+
+const CheckoutBoxBuyInfo = styled("div")(({ theme }) => ({
+  gridColumn: "8 / 12",
+  gridRow: "1 / 6",
+
+  background: "#F2F2F2",
+  borderRadius: "8px",
+  boxShadow: "0 0 10px 2px rgba(0,0,0, 0.2)",
+
+  [theme.breakpoints.down("sm")]: {
+    height: "100%",
+    width: "95%",
+  },
+  [theme.breakpoints.up("sm")]: {
+    height: "100%",
+    width: "80%",
+  },
+
+  [theme.breakpoints.up("md")]: {
+    height: "100%",
+    width: "30%",
+  },
+  [theme.breakpoints.up("lg")]: {
+    height: "100%",
+    width: "100%",
+  },
+}));
+
+const ItemBoxCheckout = styled("div")(({ theme }) => ({
+  [theme.breakpoints.down("sm")]: {
+    width: "95%",
+  },
+  [theme.breakpoints.up("sm")]: {
+    width: "80%",
+  },
+
+  [theme.breakpoints.up("md")]: {
+    width: "30%",
+  },
+  [theme.breakpoints.up("lg")]: {
+    width: "100%",
+
+    img: {
+      width: "150px",
+    },
   },
 }));
 
 export function CheckoutPage() {
+  const { user } = useAuth();
+
   const calculateTotal = (items: CartItemType[]) =>
     items.reduce((ack: number, item) => ack + item.amount * item.price, 0);
   let myCartItems = useContext(CartContext);
@@ -53,55 +162,94 @@ export function CheckoutPage() {
 
   return (
     <>
-      <CheckoutWrapper>
-        <div>
-          <h1>MY CHECKOUT PAGE</h1>
-        </div>
-
-        {myCartItems.length
-          ? myCartItems[0].length > 0
-            ? myCartItems[0].map((e: any) => (
-                <>
-                  <span> {e.title} </span> <span> R$ {e.price} </span>
-                </>
-              ))
-            : "não temos items"
-          : "sem items no carrinho"}
-
-        {myCartItems.length ? (
-          myCartItems[0].length > 0 ? (
-            <h2> Total : R${calculateTotal(myCartItems[0])},00 </h2>
+      <CheckoutMain>
+        <CheckoutBoxLoginInfo>
+          {user ? (
+            true
           ) : (
-            "não temos items"
-          )
-        ) : (
-          "sem items no carrinho"
-        )}
-        <Button
-          onClick={() => {
-            myCartItems.length
-              ? myCartItems[0].length > 0
-                ? setCheckoutData({
-                    cartId: 1,
-                    cartItems: myCartItems[0],
-                    total: calculateTotal(myCartItems[0]).toString(),
-                    PaymentMethod: "2",
-                  })
-                : console.log("não temos items")
-              : console.log("sem items no carrinho");
-          }}
-        >
-          Finalizar Compra
-        </Button>
+            <p>
+              Você não está logado !{" "}
+              <Link to="/login">Clique aqui para efetuar o login</Link>
+            </p>
+          )}
+        </CheckoutBoxLoginInfo>
 
-        <Button
-          onClick={() => {
-            console.log(checkoutData);
-          }}
-        >
-          Ver itens enviados
-        </Button>
-      </CheckoutWrapper>
+        <CheckoutBoxBuyInfo>
+          <div>Suas compras!</div>
+          {myCartItems.length
+            ? myCartItems[0].length > 0
+              ? myCartItems[0].map((e: any) => (
+                  <>
+                    <ItemBoxCheckout>
+                      <span> {e.title} </span> <span> R$ {e.price} </span>
+                      <img src={e.image} alt={e.title} />
+                    </ItemBoxCheckout>
+                  </>
+                ))
+              : "não temos items"
+            : "sem items no carrinho"}
+
+          {myCartItems.length ? (
+            myCartItems[0].length > 0 ? (
+              <h2> Total : R${calculateTotal(myCartItems[0])},00 </h2>
+            ) : (
+              "não temos items"
+            )
+          ) : (
+            "sem items no carrinho"
+          )}
+        </CheckoutBoxBuyInfo>
+        <CheckoutWrapper>
+          <div>
+            <h1>MY CHECKOUT PAGE</h1>
+          </div>
+
+          {myCartItems.length
+            ? myCartItems[0].length > 0
+              ? myCartItems[0].map((e: any) => (
+                  <>
+                    <span> {e.title} </span> <span> R$ {e.price} </span>
+                  </>
+                ))
+              : "não temos items"
+            : "sem items no carrinho"}
+
+          {myCartItems.length ? (
+            myCartItems[0].length > 0 ? (
+              <h2> Total : R${calculateTotal(myCartItems[0])},00 </h2>
+            ) : (
+              "não temos items"
+            )
+          ) : (
+            "sem items no carrinho"
+          )}
+          <Button
+            onClick={() => {
+              myCartItems.length
+                ? myCartItems[0].length > 0
+                  ? setCheckoutData({
+                      cartId: 1,
+                      cartItems: myCartItems[0],
+                      total: calculateTotal(myCartItems[0]).toString(),
+                      PaymentMethod: "2",
+                    })
+                  : console.log("não temos items")
+                : console.log("sem items no carrinho");
+            }}
+          >
+            Finalizar Compra
+          </Button>
+          <div>Informações do cadastro e pagamento</div>
+          <Button
+            onClick={() => {
+              console.log(checkoutData);
+            }}
+          >
+            Ver itens enviados
+          </Button>
+        </CheckoutWrapper>
+      </CheckoutMain>
+      <Footer />
     </>
   );
 }
