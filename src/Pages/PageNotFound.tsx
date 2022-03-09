0baba37653704a5dc153a108bad2e1d8357a07f1
vinisp <aragao93.vinicius@gmail.com>
2022-03-09 15:40:56 -0300

    page not found and yup

--- src/Pages/PageNotFound.tsx
+++ src/Pages/PageNotFound.tsx
@@ -1,7 +1,38 @@
+import { Typography } from "@mui/material";
+
+import { styled } from "@mui/material/styles";
+
+const PageNotFoundContainer = styled("div")(({ theme }) => ({
+  padding: "60px 0",
+  background: "#f2f2f2",
+
+  [theme.breakpoints.down("sm")]: {
+    height: "450px",
+    width: "95%",
+  },
+  [theme.breakpoints.up("sm")]: {
+    height: "450px",
+    width: "80%",
+  },
+
+  [theme.breakpoints.up("md")]: {
+    height: "450px",
+    width: "30%",
+  },
+  [theme.breakpoints.up("lg")]: {
+    height: "900px",
+    width: "100%",
+  },
+}));
+
 export function PageNotFound() {
   return (
     <>
-      <h1>Page Not found</h1>
+      <PageNotFoundContainer>
+        <Typography sx={{ color: "red" }} variant="h1" textAlign={"center"}>
+          Page Not found
+        </Typography>
+      </PageNotFoundContainer>
     </>
   );
 }
