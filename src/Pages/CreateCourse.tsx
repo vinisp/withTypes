6e59cbd6873ed032b585799d1182dabbfc70e0ff
vinisp <aragao93.vinicius@gmail.com>
2022-03-14 13:30:41 -0300

    drag and drop course

--- src/Pages/CreateCourse.tsx
+++ src/Pages/CreateCourse.tsx
@@ -5,6 +5,8 @@ import { useState } from "react";
 
 import { styled, ThemeProvider, createTheme } from "@mui/material/styles";
 
+import "./styles/CreateCourse.css";
+
 const theme = createTheme({
   typography: {
     fontFamily: "Open Sans, sans-serif",
@@ -63,7 +65,7 @@ const theme = createTheme({
 });
 
 const Container = styled("div")(({ theme }) => ({
-  marginTop: "",
+  marginBottom: "15px",
   [theme.breakpoints.down("sm")]: {
     display: "flex",
     flexDirection: "column",
@@ -86,21 +88,88 @@ const Container = styled("div")(({ theme }) => ({
   [theme.breakpoints.up("lg")]: {
     height: "auto",
     display: "flex",
-    background: "#f2f2f2",
+    color: "white",
+    border: "solid 1px white",
     padding: "60px",
   },
+}));
+
+const MyParagraphs = styled("div")(({ theme }) => ({
+  border: "solid 2px black",
+}));
+const CardParagraph = styled("div")(({ theme }) => ({
+  border: "solid 2px black",
 }));
 
 export function CreateCourse() {
   const { user } = useAuth();
   const [modulesTitle, setModulesTitle] = useState("");
   const [textModule, setTextModule] = useState("");
+  const [listTextModule, setListTextModule] = useState([""]);
   const [item, setItem] = useState("");
   const [itemList, setItemList] = useState([""]);
+
+  const paragraphs = document.querySelectorAll(".paragraphs");
+  const dropZones = document.querySelectorAll(".dropZone");
+
+  paragraphs.forEach((paragraph) => {
+    paragraph.addEventListener("dragstart", dragStart);
+    paragraph.addEventListener("drag", drag);
+    paragraph.addEventListener("dragend", dragEnd);
+  });
+
+  function dragStart(this: any) {
+    // console.log("você usou dragStart");
+
+    dropZones.forEach((dropZone) => dropZone.classList.add("highlight"));
+    this.classList.add("is-dragging");
+  }
+
+  function drag() {
+    // console.log("você está arrastando");
+  }
+
+  function dragEnd(this: any) {
+    // console.log("Você parou de arrastar");
+
+    dropZones.forEach((dropZone) => dropZone.classList.remove("highlight"));
+    this.classList.remove("is-dragging");
+  }
+
+  dropZones.forEach((dropZone) => {
+    dropZone.addEventListener("dragenter", dragenter);
+    dropZone.addEventListener("dragover", dragover);
+    dropZone.addEventListener("dragleave", dragleave);
+    dropZone.addEventListener("drop", drop);
+  });
+
+  function dragenter() {
+    console.log("entrou na zona");
+  }
+
+  function dragover(this: any) {
+    this.classList.add("over");
+    const elementBeignDragged = document.querySelector(".is-dragging");
+
+    this.appendChild(elementBeignDragged);
+  }
+
+  function dragleave(this: any) {
+    this.classList.remove("over");
+  }
+
+  function drop(this: any) {
+    this.classList.remove("over");
+  }
 
   const addItem = () => {
     setItemList([item].concat(itemList));
     setItem("");
+  };
+
+  const addParagraph = () => {
+    setListTextModule([textModule].concat(listTextModule));
+    setTextModule("");
   };
 
   if (!user) {
@@ -119,30 +188,54 @@ export function CreateCourse() {
     <>
       <ThemeProvider theme={theme}>
         <Container>
-          <h1>{modulesTitle}</h1>
+          <h1>Criando o seu curso</h1>
+          <p>
+            Nessa página você consegue criar seu curso, com textos, imagens,
+            vídeos e questionários
+          </p>
+          <h2>{modulesTitle}</h2>
 
-          <p> {textModule} </p>
-          <input
-            type="text"
-            onChange={(event) => setModulesTitle(event.target.value)}
-          />
-          <textarea onChange={(event) => setTextModule(event.target.value)} />
-          <input
-            type="text"
-            placeholder="Item"
-            value={item}
-            name="item"
-            onChange={(e) => setItem(e.target.value)}
-          />
+          <div>
+            <h3>Escolha o título do módulo </h3>
+            <input
+              type="text"
+              onChange={(event) => setModulesTitle(event.target.value)}
+            />
+          </div>
+          <div>
+            <h3>Insira o paragráfo</h3>
+            <textarea onChange={(event) => setTextModule(event.target.value)} />
+            <button onClick={addParagraph}>Salvar Texto</button>
+          </div>
+          <div>
+            <h3>Adicione URLS de vídeos</h3>
+            <input
+              type="text"
+              placeholder="Item"
+              value={item}
+              name="item"
+              onChange={(e) => setItem(e.target.value)}
+            />
 
-          <button onClick={addItem}>Adicionar Item</button>
-
+            <button onClick={addItem}>Adicionar Item</button>
+          </div>
           <button onClick={() => ChangeValue()}>Save</button>
-          <ul>
-            {itemList.map((e) => (
-              <li> {e} </li>
+        </Container>
+        <Container>
+          <h2>Organizando seu módulo</h2>
+          <h2>{modulesTitle}</h2>
+          <MyParagraphs>
+            <h3>Meus Parágrafos</h3>
+            {listTextModule.map((e) => (
+              <CardParagraph draggable="true" className="paragraphs">
+                {e}
+              </CardParagraph>
             ))}
-          </ul>
+          </MyParagraphs>
+        </Container>
+
+        <Container className="dropZone">
+          <h1>Layout do Curso</h1>
         </Container>
       </ThemeProvider>
     </>
