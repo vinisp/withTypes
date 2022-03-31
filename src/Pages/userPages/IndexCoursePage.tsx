39ac005c85afee9cc449b6bae3e2b542049d1457
vinisp <aragao93.vinicius@gmail.com>
2022-03-31 20:34:49 -0300

    Layout geral

--- src/Pages/userPages/IndexCoursePage.tsx
+++ src/Pages/userPages/IndexCoursePage.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useAuth } from "../../hooks/useAuth";
 
 import axios from "axios";
@@ -8,7 +8,7 @@ import { useHistory } from "react-router-dom";
 
 import { styled } from "@mui/material/styles";
 import { TextField, Button, Box, Modal, Typography } from "@mui/material/";
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { DataGrid, GridColDef, GridRenderCellParams } from "@mui/x-data-grid";
 
 import { Footer } from "../../Components/widgets/Footer";
 
@@ -25,37 +25,8 @@ const APIURL = "https://deppback.herokuapp.com/";
 const boxShadowConfig =
   "0px 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%)";
 
-const CourseCard = styled("div")(({ theme }) => ({
-  backgroundColor: "#f2f2f2",
-  border: "solid 1px silver",
-  display: "flex",
-  flexDirection: "column",
-  gap: "5px",
-
-  [theme.breakpoints.down("sm")]: {},
-  [theme.breakpoints.up("sm")]: {},
-
-  [theme.breakpoints.up("md")]: {},
-  [theme.breakpoints.up("lg")]: {},
-}));
-
-const CourseCardBox = styled("div")(({ theme }) => ({
-  backgroundColor: "#f2f2f2",
-  padding: "15px",
-  border: "solid 1px green",
-  display: "flex",
-  flexWrap: "wrap",
-  gap: "15px",
-
-  [theme.breakpoints.down("sm")]: {},
-  [theme.breakpoints.up("sm")]: {},
-
-  [theme.breakpoints.up("md")]: {},
-  [theme.breakpoints.up("lg")]: {},
-}));
-
 const MainBox = styled("div")(({ theme }) => ({
-  paddingTop: "120px",
+  paddingTop: "80px",
   background: "#f2f2f2",
   display: "flex",
   flexWrap: "wrap",
@@ -78,7 +49,7 @@ const MainInfoBox = styled("div")(({ theme }) => ({
   flexWrap: "wrap",
   order: "2",
   gap: "15px",
-  padding: "15px",
+  padding: "15px 0",
   justifyContent: "center",
   marginBottom: "15px",
 
@@ -92,11 +63,13 @@ const MainInfoBox = styled("div")(({ theme }) => ({
 const SideBar = styled("div")(({ theme }) => ({
   display: "flex",
   flexDirection: "column",
-  padding: 0,
+  padding: "15px 35px",
   alignItems: "flex-start",
   background: "#f2f2f2",
-  flex: "0 0 15%",
-  height: "600px",
+  borderRight: "solid 1px green",
+
+  flex: "0 0 20%",
+  height: "750px",
 
   [theme.breakpoints.down("sm")]: {},
   [theme.breakpoints.up("sm")]: {},
@@ -106,11 +79,11 @@ const SideBar = styled("div")(({ theme }) => ({
 }));
 
 const BoxAllInfo = styled("div")(({ theme }) => ({
-  border: "0",
   boxShadow: boxShadowConfig,
   borderRadius: "8px",
-  flex: "0 0 55%",
-  minHeight: "200px",
+  flex: "0 0 60%",
+  minHeight: "350px",
+  padding: "35px 0",
 
   [theme.breakpoints.down("sm")]: {},
   [theme.breakpoints.up("sm")]: {},
@@ -135,7 +108,8 @@ const BottomSections = styled("div")(({ theme }) => ({
   border: "0",
   boxShadow: boxShadowConfig,
   borderRadius: "8px",
-  flex: "0 0 80%",
+  flex: "0 0 86%",
+  height: "auto",
   [theme.breakpoints.down("sm")]: {},
   [theme.breakpoints.up("sm")]: {},
 
@@ -159,93 +133,39 @@ const LineDetailsBox = styled("div")(({ theme }) => ({
   [theme.breakpoints.up("lg")]: {},
 }));
 
+const PhotoUser = styled("div")(({ theme }) => ({
+  border: "solid 1px black",
+  height: "120px",
+  width: "120px",
+  borderRadius: "50%",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  [theme.breakpoints.down("sm")]: {},
+  [theme.breakpoints.up("sm")]: {},
+
+  [theme.breakpoints.up("md")]: {},
+  [theme.breakpoints.up("lg")]: {},
+}));
+
+const PhotoAndNameWrapper = styled("div")(({ theme }) => ({
+  flex: "0 0 25%",
+  width: "100%",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  [theme.breakpoints.down("sm")]: {},
+  [theme.breakpoints.up("sm")]: {},
+
+  [theme.breakpoints.up("md")]: {},
+  [theme.breakpoints.up("lg")]: {},
+}));
+
 const SelectInfo = styled("select")(({ theme }) => ({
   width: "100%",
 }));
 
 // DaTA GRID
-
-const columns: GridColDef[] = [
-  { field: "id", headerName: "ID", width: 90 },
-  {
-    field: "courseName",
-    headerName: "Nome do curso",
-    width: 150,
-    editable: true,
-  },
-  {
-    field: "category",
-    headerName: "Categoria",
-    width: 150,
-    editable: true,
-  },
-  {
-    field: "price",
-    headerName: "Preço",
-    width: 110,
-    editable: true,
-  },
-  {
-    field: "totalSell",
-    headerName: "Total de vendas",
-    width: 120,
-    editable: true,
-  },
-];
-
-const rows = [
-  {
-    id: 1,
-    category: "Categoria 1",
-    courseName: "Curso 1",
-    price: 35.59,
-    totalSell: 45,
-  },
-  {
-    id: 2,
-    category: "Lannister",
-    courseName: "Cersei",
-    price: 42,
-    totalSell: 45,
-  },
-  {
-    id: 3,
-    category: "Lannister",
-    courseName: "Jaime",
-    price: 45,
-    totalSell: 45,
-  },
-  { id: 4, category: "Stark", courseName: "Arya", price: 16, totalSell: 45 },
-  {
-    id: 5,
-    category: "Targaryen",
-    courseName: "Daenerys",
-    price: 78,
-    totalSell: 45,
-  },
-  {
-    id: 6,
-    category: "Melisandre",
-    courseName: "Odds",
-    price: 150,
-    totalSell: 45,
-  },
-  {
-    id: 7,
-    category: "Clifford",
-    courseName: "Ferrara",
-    price: 44,
-    totalSell: 45,
-  },
-  {
-    id: 8,
-    category: "Frances",
-    courseName: "Rossini",
-    price: 36,
-    totalSell: 45,
-  },
-  { id: 9, category: "Roxie", courseName: "Harvey", price: 65, totalSell: 45 },
-];
 
 export function IndexCourse() {
   const [myCourses, setMyCourses] = useState<any[]>([]);
@@ -256,7 +176,128 @@ export function IndexCourse() {
     return <Redirect to="/login" />;
   }
 
+  const columns: GridColDef[] = [
+    { field: "id", headerName: "ID", width: 50 },
+    {
+      field: "courseName",
+      headerName: "Nome do curso",
+      width: 160,
+      editable: false,
+    },
+    {
+      field: "category",
+      headerName: "Categoria",
+      width: 100,
+      editable: false,
+    },
+    {
+      field: "price",
+      headerName: "Preço",
+      width: 70,
+      editable: false,
+    },
+    {
+      field: "totalSell",
+      headerName: "Total de vendas",
+      width: 120,
+      editable: false,
+    },
+    {
+      field: "owner",
+      headerName: "Dono",
+      width: 70,
+      renderCell: (params: GridRenderCellParams<any>) => (
+        <Typography>{params.row.owner === user_id ? "Sim" : "Não"}</Typography>
+      ),
+    },
+    {
+      field: "edit",
+      headerName: "Editar",
+      renderCell: (params: GridRenderCellParams<any>) => (
+        <Button
+          variant="text"
+          color="primary"
+          size="small"
+          onClick={() => {
+            history.push(`/editcourse/${params.row.id}`);
+          }}
+        >
+          Editar
+        </Button>
+      ),
+    },
+    {
+      field: "delete",
+      headerName: "Deletar",
+      renderCell: (params: GridRenderCellParams<any>) => (
+        <Button
+          variant="text"
+          color="error"
+          onClick={async () => {
+            try {
+              axios
+                .delete(`${APIURL}course/delete/${params.row.id}`)
+                .then((response) => console.log("deletado", response))
+                .catch((err) => console.error(err));
+            } catch (err) {
+              console.error(err);
+            }
+            try {
+              axios
+                .delete(`${APIURL}champters/delete/${params.row.id}`)
+                .then((response) => console.log("deletado", response))
+                .catch((err) => console.error(err));
+            } catch (err) {
+              console.error(err);
+            }
+            try {
+              axios
+                .delete(`${APIURL}elements/delete/${params.row.id}`)
+                .then((response) => console.log("deletado", response))
+                .catch((err) => console.error(err));
+            } catch (err) {
+              console.error(err);
+            }
+
+            try {
+              const getCourses = await axios
+                .get(`${APIURL}search/${user_id}`)
+                .then((response) => {
+                  setMyCourses(response.data);
+                });
+              return getCourses;
+            } catch (err) {
+              console.error(err);
+            }
+          }}
+        >
+          Deletar
+        </Button>
+      ),
+    },
+  ];
+
   const user_id = user?.id;
+
+  function RenderCourseInTable() {
+    useEffect(() => {
+      axios.get(`${APIURL}search/${user_id}`).then((response) => {
+        setMyCourses(response.data);
+      });
+    }, []);
+  }
+
+  RenderCourseInTable();
+
+  const rows = myCourses.map((e) => ({
+    id: e.course_id,
+    owner: e.created_by,
+    courseName: e.name,
+    category: e.category,
+    level: e.level,
+    totalSell: 85,
+    price: e.price,
+  }));
 
   function MainCourseInformations() {
     const [name, setName] = useState("");
@@ -276,16 +317,13 @@ export function IndexCourse() {
 
     return (
       <>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "flex-start",
-            padding: "0 0",
-            width: "100%",
-            gap: "8px",
-          }}
-        >
+        <SideBar>
+          <PhotoAndNameWrapper>
+            <PhotoUser>Foto</PhotoUser>
+            <Typography variant="caption" sx={{ fontWeight: 600 }}>
+              {user?.email}
+            </Typography>
+          </PhotoAndNameWrapper>
           <Button
             sx={{ width: "100%" }}
             variant="contained"
@@ -293,32 +331,9 @@ export function IndexCourse() {
           >
             Criar Novo Curso
           </Button>
-          <Button
-            sx={{ width: "100%" }}
-            variant="contained"
-            onClick={async () => {
-              try {
-                const getCourses = await axios
-                  .get(`${APIURL}search/${user_id}`)
-                  .then((response) => {
-                    setMyCourses(response.data);
-                  });
-                return getCourses;
-              } catch (err) {
-                console.error(err);
-              }
-            }}
-          >
-            Meus Cursos Desenvolvidos
-          </Button>
-          <Button sx={{ width: "100%" }} variant="contained">
-            Meus Cursos Comprados
-          </Button>
-          <Button sx={{ width: "100%" }} variant="contained">
-            Meus Cursos Promovidos
-          </Button>
-        </Box>
-
+          <Typography>Dados do Perfil</Typography>
+          <div>Lista de interresses</div>
+        </SideBar>
         <Modal
           open={open}
           onClose={handleClose}
@@ -424,6 +439,15 @@ export function IndexCourse() {
       <MainBox>
         <MainInfoBox>
           <BoxAllInfo>
+            <Typography
+              sx={{
+                flex: "0 0 5%",
+                textAlign: "center",
+              }}
+              variant="h6"
+            >
+              Lista de Cursos
+            </Typography>
             <DataGrid
               rows={rows}
               columns={columns}
@@ -433,18 +457,41 @@ export function IndexCourse() {
               disableSelectionOnClick
             />
           </BoxAllInfo>
-          <BalanceInfo>Depositos recentes</BalanceInfo>
+          <BalanceInfo>
+            <Typography variant="h5" color="green">
+              Depósitos
+            </Typography>
+            <Typography
+              variant="h6"
+              sx={{ fontWeight: "600", fontSize: "36px" }}
+            >
+              R$ 560,95
+            </Typography>
+            <Typography color="rgb(115,115,116)">
+              Atualizado em 31/03/2022
+            </Typography>
+            <Typography sx={{ position: "absolute", top: "90%" }}>
+              Ver detalhes
+            </Typography>
+          </BalanceInfo>
+
           <BottomSections>
             <LineDetailsBox>
               <span>Meus Cursos Desenvolvidos</span>
               <span>% de vendas</span>
               <span>Valor em $$$</span>
             </LineDetailsBox>
+            <span>Informar Quantidade</span>
+            <span>Porcentagem de vendas</span>
+            <span>Valor</span>
             <LineDetailsBox>
               <span>Meus Cursos Promovidos</span>
               <span>% de vendas</span>
               <span>Valor em $$$</span>
             </LineDetailsBox>
+            <span>Informar Quantidade</span>
+            <span>Porcentagem de vendas</span>
+            <span>Valor</span>
           </BottomSections>
           <BottomSections>
             <LineDetailsBox>
@@ -452,91 +499,15 @@ export function IndexCourse() {
               <span>Categoria</span>
               <span>Acessar</span>
             </LineDetailsBox>
+            <span>Nome</span>
+            <span>Categoria </span>
+            <span>Acesso</span>
           </BottomSections>
         </MainInfoBox>
-        <SideBar>
-          <ul>{MainCourseInformations()}</ul>
-        </SideBar>
-        <div>
-          {myCourses.length > 0 ? (
-            <CourseCardBox>
-              {myCourses.map((e) => (
-                <>
-                  <CourseCard>
-                    <div>Nome do Curso: {e.name} </div>
-                    <div>Preço: {e.price} </div>
-                    <div>Dificuldade: {e.level}</div>
-                    <div>Categoria: {e.category} </div>
-                    <div>
-                      <Button
-                        variant="outlined"
-                        onClick={() =>
-                          history.push(`/editcourse/${e.course_id}`)
-                        }
-                      >
-                        Editar Curso
-                      </Button>
-                      <Button
-                        variant="outlined"
-                        color="error"
-                        onClick={async () => {
-                          try {
-                            axios
-                              .delete(`${APIURL}course/delete/${e.course_id}`)
-                              .then((response) =>
-                                console.log("deletado", response)
-                              )
-                              .catch((err) => console.error(err));
-                          } catch (err) {
-                            console.error(err);
-                          }
-                          try {
-                            axios
-                              .delete(
-                                `${APIURL}champters/delete/${e.course_id}`
-                              )
-                              .then((response) =>
-                                console.log("deletado", response)
-                              )
-                              .catch((err) => console.error(err));
-                          } catch (err) {
-                            console.error(err);
-                          }
-                          try {
-                            axios
-                              .delete(`${APIURL}elements/delete/${e.course_id}`)
-                              .then((response) =>
-                                console.log("deletado", response)
-                              )
-                              .catch((err) => console.error(err));
-                          } catch (err) {
-                            console.error(err);
-                          }
 
-                          try {
-                            const getCourses = await axios
-                              .get(`${APIURL}search/${user_id}`)
-                              .then((response) => {
-                                setMyCourses(response.data);
-                              });
-                            return getCourses;
-                          } catch (err) {
-                            console.error(err);
-                          }
-                        }}
-                      >
-                        Deletar Curso
-                      </Button>
-                    </div>
-                  </CourseCard>
-                </>
-              ))}
-            </CourseCardBox>
-          ) : (
-            false
-          )}
-        </div>
+        {MainCourseInformations()}
       </MainBox>
+
       <Footer />
     </>
   );
