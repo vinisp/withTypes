1c5b096453b682aeff92af41bfd8d9034123245d
vinisp <aragao93.vinicius@gmail.com>
2022-04-07 20:12:02 -0300

    o teste do plauer

--- src/Pages/paginas_testes/testar_componentes.tsx
+++ src/Pages/paginas_testes/testar_componentes.tsx
@@ -4,6 +4,7 @@ import ReactPlayer from "react-player/lazy";
 export function TestarComponentes() {
   return (
     <>
+      <ReactPlayer url="https://www.youtube.com/watch?v=ysz5S6PUM-U" />
       <Box
         sx={{
           backgroundColor: "#F2f2f2f2",
@@ -11,7 +12,6 @@ export function TestarComponentes() {
         }}
       >
         <h1>Página para teste de componentes</h1>
-        <ReactPlayer url="https://www.youtube.com/watch?v=ysz5S6PUM-U" />
       </Box>
     </>
   );
