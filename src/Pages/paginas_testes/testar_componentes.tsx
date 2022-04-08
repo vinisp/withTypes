b00942348f053ee7f0738185f24ecf59940cc6ba
vinisp <aragao93.vinicius@gmail.com>
2022-04-07 22:07:22 -0300

    a espera de um novo pl

--- src/Pages/paginas_testes/testar_componentes.tsx
+++ src/Pages/paginas_testes/testar_componentes.tsx
@@ -1,31 +1,29 @@
 //import React from "react";
 
 import { Box } from "@mui/material";
-
-import ReactPlayer from "react-player/youtube";
-
-type MyProps = {
-  children: JSX.Element;
-};
-
-function Player(props: MyProps) {
-  return <div>{props.children} </div>;
-}
+import Plyr from "plyr-react";
 
 export function TestarComponentes() {
   return (
     <>
       <Box
         sx={{
-          backgroundColor: "#F2f2f2f2",
+          backgroundColor: "#F2f2f2",
           paddingTop: "80px",
         }}
       >
         <h1>Página para teste de componentes</h1>
-
-        <Player>
-          <ReactPlayer url="https://www.youtube.com/watch?v=ysz5S6PUM-U" />
-        </Player>
+        <Plyr
+          source={{
+            type: "video",
+            sources: [
+              {
+                src: "268402641",
+                provider: "vimeo",
+              },
+            ],
+          }}
+        />
       </Box>
     </>
   );
