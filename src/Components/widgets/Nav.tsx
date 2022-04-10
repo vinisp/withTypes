ef048e0af28d2375d2ad4b8bcee7435469c67130
vinisp <aragao93.vinicius@gmail.com>
2022-04-10 11:56:26 -0300

    mobile

--- src/Components/widgets/Nav.tsx
+++ src/Components/widgets/Nav.tsx
@@ -174,53 +174,6 @@ const MenuDesktop = styled("nav")(({ theme }) => ({
   },
 }));
 
-function PositionedMenu() {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  return (
-    <div>
-      <Button
-        id="demo-positioned-button"
-        aria-controls={open ? "demo-positioned-menu" : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? "true" : undefined}
-        onClick={handleClick}
-      >
-        <MenuIcon />
-      </Button>
-      <Menu
-        id="demo-positioned-menu"
-        aria-labelledby="demo-positioned-button"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        anchorOrigin={{
-          vertical: "top",
-          horizontal: "left",
-        }}
-        transformOrigin={{
-          vertical: "top",
-          horizontal: "left",
-        }}
-        sx={{
-          zIndex: "9999",
-        }}
-      >
-        <MenuItem onClick={handleClose}>Profile</MenuItem>
-        <MenuItem onClick={handleClose}>My account</MenuItem>
-        <MenuItem onClick={handleClose}>Logout</MenuItem>
-      </Menu>
-    </div>
-  );
-}
-
 function Nav() {
   const { user } = useAuth();
 
@@ -232,6 +185,104 @@ function Nav() {
       .catch((error) => {
         console.log(error);
       });
+
+  function PositionedMenu() {
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+      setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+
+    return (
+      <div>
+        <Button
+          id="demo-positioned-button"
+          aria-controls={open ? "demo-positioned-menu" : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+          onClick={handleClick}
+        >
+          <MenuIcon />
+        </Button>
+        <Menu
+          id="demo-positioned-menu"
+          aria-labelledby="demo-positioned-button"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "left",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "left",
+          }}
+          sx={{
+            zIndex: "9999",
+          }}
+        >
+          <ul>
+            {user ? (
+              <>
+                <li>
+                  <MenuItem onClick={handleClose}>
+                    <Link to="/">Home</Link>
+                  </MenuItem>
+                </li>
+                <li>
+                  <MenuItem onClick={handleClose}>
+                    <Link to="/course">Central</Link>
+                  </MenuItem>
+                </li>
+                <li>
+                  <MenuItem onClick={handleClose}>
+                    <Link to="/store">Loja</Link>
+                  </MenuItem>
+                </li>
+                <li>
+                  <MenuItem onClick={handleClose}>
+                    <CartNav />
+                  </MenuItem>
+                </li>
+                <li>
+                  <MenuItem onClick={handleClose}>
+                    <Link to="/profile">Perfil</Link>
+                  </MenuItem>
+                </li>
+                <li>
+                  <MenuItem onClick={handleClose}>
+                    <StyledButton onClick={LogOut}>Logout</StyledButton>
+                  </MenuItem>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/store">Loja</Link>
+                </li>
+                <li>
+                  <CartNav />
+                </li>
+                <li>
+                  <Link to="login">Login</Link>
+                </li>
+                <li>
+                  <Link to="/register">Cadastrar</Link>
+                </li>
+              </>
+            )}
+          </ul>
+        </Menu>
+      </div>
+    );
+  }
 
   return (
     <>
