7da0b91f28e79c4eabc5538b91c4c2a568582b90
vinisp <aragao93.vinicius@gmail.com>
2022-04-16 18:31:34 -0300

    informacoes promocionais do curso #2

--- src/Pages/userPages/EditIndexCoursePage.tsx
+++ src/Pages/userPages/EditIndexCoursePage.tsx
@@ -39,21 +39,70 @@ const FieldBox = styled("div")(({ theme }) => ({
   [theme.breakpoints.up("lg")]: {},
 }));
 
+const FieldBoxHalf = styled("div")(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  gap: "4px",
+
+  label: {
+    color: "darkgreen",
+    fontWeight: "600",
+  },
+
+  [theme.breakpoints.down("sm")]: {},
+  [theme.breakpoints.up("sm")]: {},
+
+  [theme.breakpoints.up("md")]: {
+    width: "30%",
+  },
+  [theme.breakpoints.up("lg")]: {
+    width: "30%",
+  },
+}));
+
+const FieldBoxHalfWrapper = styled("div")(({ theme }) => ({
+  display: "flex",
+
+  gap: "4px",
+
+  width: "100%",
+
+  label: {
+    color: "darkgreen",
+    fontWeight: "600",
+  },
+
+  [theme.breakpoints.down("sm")]: {
+    flexDirection: "column",
+  },
+  [theme.breakpoints.up("sm")]: {
+    flexDirection: "column",
+  },
+
+  [theme.breakpoints.up("md")]: {
+    flexDirection: "row",
+  },
+  [theme.breakpoints.up("lg")]: {
+    gap: "16px",
+    flexDirection: "row",
+  },
+}));
+
 export function IndexCoursePage() {
   const [userName, setUserName] = useState<string>("");
-  const [resume, setResume] = useState<string>("");
-  const [facebookLink, setFacebookLink] = useState<string>("");
-  const [instagramLink, setInstagramLink] = useState<string>("");
-  const [twitterLink, setTwitterLink] = useState<string>("");
-  const [telegram, setTelegram] = useState<string>("");
-  const [whatsapp, setWhatsapp] = useState<string>("");
+  const [mainInfosFirst, setMainInfosFirst] = useState<string>("");
+  const [resume1, setResume1] = useState<string>("");
+  const [resume2, setResume2] = useState<string>("");
+  const [resume3, setResume3] = useState<string>("");
+  const [maisDetalhes, setMaisDetalhes] = useState<string>("");
+  const [planoDeEnsino, setPlanoDeEnsino] = useState<string>("");
 
   return (
     <>
       <FormWrapper>
-        <h1>Minha página de edição do perfil</h1>
+        <h1>Edição do Curso</h1>
         <FieldBox>
-          <label>Nome do usuário</label>
+          <label>Nome do curso</label>
           <TextField
             id="outlined-name"
             value={userName}
@@ -65,11 +114,11 @@ export function IndexCoursePage() {
         </FieldBox>
 
         <FieldBox>
-          <label>Resumo</label>
+          <label>Resumo do Curso</label>
           <TextareaAutosize
             id="outlined-name"
             minRows={10}
-            value={resume}
+            value={mainInfosFirst}
             style={{
               width: "100%",
               maxWidth: "100%",
@@ -77,93 +126,199 @@ export function IndexCoursePage() {
               padding: "20px",
             }}
             onChange={(event) => {
-              resume.length <= 255
-                ? setResume(event.target.value)
-                : resume.length > 255
-                ? resume.split("", 255 - resume.length)
+              mainInfosFirst.length <= 255
+                ? setMainInfosFirst(event.target.value)
+                : mainInfosFirst.length > 255
+                ? mainInfosFirst.split("", 255 - mainInfosFirst.length)
                 : console.log(false);
-              setResume(event.target.value);
+              setMainInfosFirst(event.target.value);
             }}
           />
           <p
             style={{
-              color: `${resume.length > 255 ? "red" : "black"}`,
+              color: `${mainInfosFirst.length > 255 ? "red" : "black"}`,
               textAlign: "right",
             }}
           >
-            Número de caracteres: {resume.length}
-            {resume.length > 255 ? " (número maxímo de caracteres 255)" : false}
+            Número de caracteres: {mainInfosFirst.length}
+            {mainInfosFirst.length > 255
+              ? " (número maxímo de caracteres 255)"
+              : false}
+          </p>
+        </FieldBox>
+        <FieldBoxHalfWrapper>
+          <FieldBoxHalf>
+            <label>Informação Promocional 1</label>
+            <TextareaAutosize
+              id="outlined-name"
+              minRows={10}
+              value={resume1}
+              style={{
+                width: "100%",
+                maxWidth: "100%",
+                minWidth: "100%",
+                padding: "20px",
+              }}
+              onChange={(event) => {
+                resume1.length <= 255
+                  ? setResume1(event.target.value)
+                  : resume1.length > 255
+                  ? resume1.split("", 255 - resume1.length)
+                  : console.log(false);
+                setResume1(event.target.value);
+              }}
+            />
+            <p
+              style={{
+                color: `${resume1.length > 255 ? "red" : "black"}`,
+                textAlign: "right",
+              }}
+            >
+              Número de caracteres: {resume1.length}
+              {resume1.length > 255
+                ? " (número maxímo de caracteres 255)"
+                : false}
+            </p>
+          </FieldBoxHalf>
+
+          <FieldBoxHalf>
+            <label>Informação Promocional 1</label>
+            <TextareaAutosize
+              id="outlined-name"
+              minRows={10}
+              value={resume2}
+              style={{
+                width: "100%",
+                maxWidth: "100%",
+                minWidth: "100%",
+                padding: "20px",
+              }}
+              onChange={(event) => {
+                resume2.length <= 255
+                  ? setResume2(event.target.value)
+                  : resume2.length > 255
+                  ? resume2.split("", 255 - resume2.length)
+                  : console.log(false);
+                setResume2(event.target.value);
+              }}
+            />
+            <p
+              style={{
+                color: `${resume2.length > 255 ? "red" : "black"}`,
+                textAlign: "right",
+              }}
+            >
+              Número de caracteres: {resume2.length}
+              {resume2.length > 255
+                ? " (número maxímo de caracteres 255)"
+                : false}
+            </p>
+          </FieldBoxHalf>
+
+          <FieldBoxHalf>
+            <label>Informação Promocional 1</label>
+            <TextareaAutosize
+              id="outlined-name"
+              minRows={10}
+              value={resume3}
+              style={{
+                width: "100%",
+                maxWidth: "100%",
+                minWidth: "100%",
+                padding: "20px",
+              }}
+              onChange={(event) => {
+                resume3.length <= 255
+                  ? setResume3(event.target.value)
+                  : resume3.length > 255
+                  ? resume3.split("", 255 - resume3.length)
+                  : console.log(false);
+                setResume3(event.target.value);
+              }}
+            />
+            <p
+              style={{
+                color: `${resume3.length > 255 ? "red" : "black"}`,
+                textAlign: "right",
+              }}
+            >
+              Número de caracteres: {resume3.length}
+              {resume3.length > 255
+                ? " (número maxímo de caracteres 255)"
+                : false}
+            </p>
+          </FieldBoxHalf>
+        </FieldBoxHalfWrapper>
+
+        <FieldBox>
+          <label>Mais Detalhes</label>
+          <TextareaAutosize
+            id="outlined-name"
+            minRows={10}
+            value={maisDetalhes}
+            style={{
+              width: "100%",
+              maxWidth: "100%",
+              minWidth: "100%",
+              padding: "20px",
+            }}
+            onChange={(event) => {
+              maisDetalhes.length <= 255
+                ? setMaisDetalhes(event.target.value)
+                : maisDetalhes.length > 255
+                ? maisDetalhes.split("", 255 - maisDetalhes.length)
+                : console.log(false);
+              setMaisDetalhes(event.target.value);
+            }}
+          />
+          <p
+            style={{
+              color: `${maisDetalhes.length > 255 ? "red" : "black"}`,
+              textAlign: "right",
+            }}
+          >
+            Número de caracteres: {maisDetalhes.length}
+            {maisDetalhes.length > 255
+              ? " (número maxímo de caracteres 255)"
+              : false}
           </p>
         </FieldBox>
 
         <FieldBox>
-          <label>Link do Facebook</label>
-          <TextField
+          <label>Plano de Ensino</label>
+          <TextareaAutosize
             id="outlined-name"
-            value={facebookLink}
+            minRows={10}
+            value={planoDeEnsino}
+            style={{
+              width: "100%",
+              maxWidth: "100%",
+              minWidth: "100%",
+              padding: "20px",
+            }}
             onChange={(event) => {
-              setFacebookLink(event.target.value);
+              planoDeEnsino.length <= 255
+                ? setPlanoDeEnsino(event.target.value)
+                : planoDeEnsino.length > 255
+                ? planoDeEnsino.split("", 255 - planoDeEnsino.length)
+                : console.log(false);
+              setPlanoDeEnsino(event.target.value);
             }}
           />
+          <p
+            style={{
+              color: `${planoDeEnsino.length > 255 ? "red" : "black"}`,
+              textAlign: "right",
+            }}
+          >
+            Número de caracteres: {planoDeEnsino.length}
+            {planoDeEnsino.length > 255
+              ? " (número maxímo de caracteres 255)"
+              : false}
+          </p>
         </FieldBox>
 
-        <FieldBox>
-          <label>Link do Instagram</label>
-          <TextField
-            id="outlined-name"
-            value={instagramLink}
-            onChange={(event) => {
-              setInstagramLink(event.target.value);
-            }}
-          />
-        </FieldBox>
-
-        <FieldBox>
-          <label>Link do Twitter</label>
-          <TextField
-            id="outlined-name"
-            value={twitterLink}
-            onChange={(event) => {
-              setTwitterLink(event.target.value);
-            }}
-          />
-        </FieldBox>
-
-        <FieldBox>
-          <label>Número do Telegram</label>
-          <TextField
-            id="outlined-name"
-            value={telegram}
-            onChange={(event) => {
-              setTelegram(event.target.value);
-            }}
-          />
-        </FieldBox>
-
-        <FieldBox>
-          <label>Número do Whatsapp</label>
-          <TextField
-            id="outlined-name"
-            value={whatsapp}
-            onChange={(event) => {
-              setWhatsapp(event.target.value);
-            }}
-          />
-        </FieldBox>
-        <Button
-          variant="contained"
-          color="success"
-          onClick={() => {
-            console.log({
-              user_name: userName,
-              resume: resume,
-              facebook_link: facebookLink,
-              twitter_link: twitterLink,
-              telegram: telegram,
-              whatsapp: whatsapp,
-            });
-          }}
-        >
+        <Button variant="contained" color="success" onClick={() => {}}>
           Salvar Dados
         </Button>
         <Link to={"/"}>
