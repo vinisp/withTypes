7f4b22eb3349c7fbb25bf84de199040cb593f943
vinisp <aragao93.vinicius@gmail.com>
2022-04-23 12:07:17 -0300

    pagina do pagamento

--- src/Pages/PayOneCourse.tsx
+++ src/Pages/PayOneCourse.tsx
@@ -1,8 +1,25 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import axios from "axios";
+
+const APIURL = "https://deppback.herokuapp.com/";
 
 export function PayOneCourse() {
+  const [course, setCourse] = useState<any[]>([]);
   let { idCourse } = useParams<any>();
+
+  function GetCourseData() {
+    useEffect(() => {
+      axios
+        .get(`${APIURL}course/${idCourse}`)
+        .then((response) => setCourse(response.data));
+    }, []);
+  }
+
+  GetCourseData();
+
   return (
     <>
       <Box
@@ -13,6 +30,11 @@ export function PayOneCourse() {
       >
         <h1>Pay One Course Page</h1>
         <h2>Comprar o Curso {idCourse} </h2>
+        {course.length > 0 ? (
+          [course[0].name, course[0].price, course[0].thumb_url]
+        ) : (
+          <Typography>Carregando </Typography>
+        )}
       </Box>
     </>
   );
