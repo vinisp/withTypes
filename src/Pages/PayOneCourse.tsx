ac2a06b5c1a29aee196c5425ea8439d99b2eb227
vinisp <aragao93.vinicius@gmail.com>
2022-04-23 14:54:12 -0300

    pagina de pagamento

--- src/Pages/PayOneCourse.tsx
+++ src/Pages/PayOneCourse.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, TextField, Button } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
 import { useParams } from "react-router-dom";
@@ -9,7 +9,7 @@ import axios from "axios";
 const APIURL = "https://deppback.herokuapp.com/";
 
 const Container = styled("div")(({ theme }) => ({
-  backgroundColor: "#f2f2f2f2",
+  backgroundColor: "#f2f2f2",
   display: "flex",
   alignItems: "flex-start",
 
@@ -35,13 +35,70 @@ const Container = styled("div")(({ theme }) => ({
 }));
 
 const Banner = styled("div")(({ theme }) => ({
-  backgroundColor: "red",
+  backgroundColor: "#0c0c0c",
   display: "flex",
-
-  alignItems: "flex-start",
+  color: "white",
+  fontSize: 80,
   gap: "10px",
   overflowX: "hidden",
-  height: "auto",
+  height: "280px",
+  flex: "0 0 100%",
+
+  [theme.breakpoints.down("sm")]: {
+    flexWrap: "wrap",
+  },
+  [theme.breakpoints.up("sm")]: {
+    flexWrap: "wrap",
+  },
+
+  [theme.breakpoints.up("md")]: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexWrap: "wrap",
+  },
+  [theme.breakpoints.up("lg")]: {
+    padding: "",
+    heigth: "480px",
+  },
+}));
+
+const Formulario = styled("div")(({ theme }) => ({
+  paddingTop: "120px",
+  paddingBottom: "80px",
+  paddingLeft: "80px",
+  paddingRight: "80px",
+  display: "flex",
+  alignItems: "flex-start",
+  gap: "10px",
+
+  flex: "0 0 70%",
+  border: "solid 1px silver",
+  marginTop: 5,
+
+  [theme.breakpoints.down("sm")]: {
+    flexWrap: "wrap",
+  },
+  [theme.breakpoints.up("sm")]: {
+    flexWrap: "wrap",
+  },
+
+  [theme.breakpoints.up("md")]: {
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "center",
+    flexWrap: "wrap",
+    paddingTop: "120px",
+    paddingBottom: "80px",
+  },
+  [theme.breakpoints.up("lg")]: {},
+}));
+
+const FormularioFields = styled("div")(({ theme }) => ({
+  backgroundColor: "#f2f2f2f2",
+  display: "flex",
+
+  gap: "12px",
   flex: "0 0 100%",
 
   [theme.breakpoints.down("sm")]: {
@@ -56,25 +113,15 @@ const Banner = styled("div")(({ theme }) => ({
     alignItems: "flex-start",
     justifyContent: "center",
     flexWrap: "wrap",
-    paddingTop: "120px",
-    paddingBottom: "80px",
   },
-  [theme.breakpoints.up("lg")]: {
-    padding: "",
-    heigth: "480px",
-  },
+  [theme.breakpoints.up("lg")]: {},
 }));
 
-const Formulario = styled("div")(({ theme }) => ({
-  backgroundColor: "green",
-  paddingTop: "120px",
-  paddingBottom: "80px",
+const Row = styled("div")(({ theme }) => ({
   display: "flex",
-  alignItems: "flex-start",
-  gap: "10px",
-  overflowX: "hidden",
-  height: "auto",
-  flex: "0 0 70%",
+  flexDirection: "column",
+  gap: "12px",
+  flex: "0 0 100%",
 
   [theme.breakpoints.down("sm")]: {
     flexWrap: "wrap",
@@ -88,8 +135,70 @@ const Formulario = styled("div")(({ theme }) => ({
     alignItems: "flex-start",
     justifyContent: "center",
     flexWrap: "wrap",
-    paddingTop: "120px",
-    paddingBottom: "80px",
+  },
+  [theme.breakpoints.up("lg")]: {},
+}));
+
+const RowOpt2 = styled("div")(({ theme }) => ({
+  display: "flex",
+  gap: "20px",
+  flex: "0 0 100%",
+
+  [theme.breakpoints.down("sm")]: {
+    flexWrap: "wrap",
+  },
+  [theme.breakpoints.up("sm")]: {
+    flexWrap: "wrap",
+  },
+
+  [theme.breakpoints.up("md")]: {
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
+    flexWrap: "wrap",
+  },
+  [theme.breakpoints.up("lg")]: {},
+}));
+
+const RowChild = styled("div")(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  gap: "12px",
+
+  [theme.breakpoints.down("sm")]: {
+    flexWrap: "wrap",
+  },
+  [theme.breakpoints.up("sm")]: {
+    flexWrap: "wrap",
+  },
+
+  [theme.breakpoints.up("md")]: {
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "center",
+    flexWrap: "wrap",
+  },
+  [theme.breakpoints.up("lg")]: {},
+}));
+
+const GroupSelect = styled("div")(({ theme }) => ({
+  display: "flex",
+
+  gap: "6px",
+  flex: "0 0 100%",
+
+  [theme.breakpoints.down("sm")]: {
+    flexWrap: "wrap",
+  },
+  [theme.breakpoints.up("sm")]: {
+    flexWrap: "wrap",
+  },
+
+  [theme.breakpoints.up("md")]: {
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "center",
+    flexWrap: "wrap",
   },
   [theme.breakpoints.up("lg")]: {},
 }));
@@ -124,6 +233,7 @@ const Detalhes = styled("div")(({ theme }) => ({
 
 export function PayOneCourse() {
   const [course, setCourse] = useState<any[]>([]);
+  const [paymentOption, setPaymentOption] = useState<string>("");
   let { idCourse } = useParams<any>();
 
   function GetCourseData() {
@@ -136,6 +246,51 @@ export function PayOneCourse() {
 
   GetCourseData();
 
+  const Credit = (
+    <FormularioFields>
+      <Row>
+        <label>Número do cartão</label>
+        <TextField fullWidth />
+      </Row>
+      <RowOpt2>
+        <RowChild sx={{ flex: "0 0 55%" }}>
+          <label>Validade</label>
+          <TextField sx={{ width: "100%" }} />
+        </RowChild>
+        <RowChild sx={{ flex: "0 0 40%" }}>
+          <label>Cód de Segurança</label>
+          <TextField sx={{ width: "100%" }} />
+        </RowChild>
+      </RowOpt2>
+      <Row>
+        <label>Nome impresso no cartão</label>
+        <TextField fullWidth />
+      </Row>
+      <Row>
+        <label>Parcelas</label>
+        <TextField fullWidth />
+      </Row>
+    </FormularioFields>
+  );
+  const Boleto = <div> Boleto: </div>;
+  const Pix = <div> Pix: </div>;
+  const PayPal = <div> PayPal: </div>;
+
+  function PaymentFormRender() {
+    if (paymentOption === "credit") {
+      return Credit;
+    }
+    if (paymentOption === "boleto") {
+      return Boleto;
+    }
+    if (paymentOption === "pix") {
+      return Pix;
+    }
+    if (paymentOption === "payPal") {
+      return PayPal;
+    }
+  }
+
   return (
     <>
       <Box
@@ -144,8 +299,6 @@ export function PayOneCourse() {
           backgroundColor: "#FFF",
         }}
       >
-        <h1>Pay One Course Page</h1>
-        <h2>Comprar o Curso {idCourse} </h2>
         {course.length > 0 ? (
           [course[0].name, course[0].price, course[0].thumb_url]
         ) : (
@@ -156,21 +309,53 @@ export function PayOneCourse() {
       <Container>
         <Banner>Banner</Banner>
         <Formulario>
-          <div className="Container do formulário">
-            <div className="MiniAndPrice">
-              <div>Miniatura</div>
-              <div>
-                <div>Nome do curso</div>
-                <div>Preço</div>
-              </div>
-            </div>
+          <div className="MiniAndPrice">
+            <div>Miniatura</div>
             <div>
-              <div>
-                <label>Nome Completo</label>
-                <input type="text" />
-              </div>
+              <div>Nome do curso</div>
+              <div>Preço</div>
             </div>
           </div>
+          <FormularioFields>
+            <Row>
+              <label>Nome Completo</label>
+              <TextField fullWidth />
+            </Row>
+            <Row>
+              <label>E-mail</label>
+              <TextField fullWidth />
+            </Row>
+            <Row>
+              <label>Celular</label>
+              <TextField fullWidth />
+            </Row>
+            <Row>
+              <label>CPF ou CNPJ</label>
+              <TextField fullWidth />
+            </Row>
+            <GroupSelect>
+              <div>
+                <Button onClick={() => setPaymentOption("credit")}>
+                  Cartão de Crédito
+                </Button>
+              </div>
+              <div>
+                <Button onClick={() => setPaymentOption("boleto")}>
+                  Boleto
+                </Button>
+              </div>
+              <div>
+                <Button onClick={() => setPaymentOption("pix")}>Pix</Button>
+              </div>
+              <div>
+                <Button onClick={() => setPaymentOption("payPal")}>
+                  PayPal
+                </Button>
+              </div>
+            </GroupSelect>
+            {PaymentFormRender()}
+          </FormularioFields>
+
           <div>Rodapé</div>
         </Formulario>
         <Detalhes>25%</Detalhes>
